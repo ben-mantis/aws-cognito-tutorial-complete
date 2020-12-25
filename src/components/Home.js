5dc774c863733ecd8a2082ee4ea921b4dfe80636
ben-mantis <75925788+ben-mantis@users.noreply.github.com>
2020-12-25 12:33:37 +0200

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,11 +4,13 @@ import HomeContent from './HomeContent';
 
 export default function Home() {
   return (
+    console.log("checking");
+    console.log(process.env.TESTING);
     <Fragment>
       <Hero />
       <div className="box cta">
         <p className="has-text-centered">
-          <span className="tag is-primary">New {console.log(process.env.TESTING)} </span> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          <span className="tag is-primary">New {console.log("checking")}{console.log(process.env.TESTING)} </span> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
         </p>
       </div>
       <HomeContent />
