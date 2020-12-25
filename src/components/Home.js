04ceb2276975af492e124c5214d8350906f705a1
ben-mantis <75925788+ben-mantis@users.noreply.github.com>
2020-12-25 12:25:28 +0200

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ export default function Home() {
       <Hero />
       <div className="box cta">
         <p className="has-text-centered">
-          <span className="tag is-primary">New {process.env.TESTING} </span> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          <span className="tag is-primary">New {console.log(process.env.TESTING)} </span> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
         </p>
       </div>
       <HomeContent />
